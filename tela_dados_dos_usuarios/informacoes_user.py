64e3a2bf840c173a2549365b6a234e149d4d6d5b
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-09 16:28:42 -0300

    Estrutura básica atualizada

---
+++ tela_dados_dos_usuarios/informacoes_user.py
@@ -0,0 +1,7 @@
+def pegar_informacoes():
+    #Cria o bando de dados e armazena o necessário
+    pass
+
+def alterar_informacoes():
+    #Consulta o banco de dados e edite o necessário
+    pass
---
+++ tela_dados_dos_usuarios/main.py
@@ -0,0 +1,24 @@
+import informacoes_user
+from profissionais import informacoes_prof
+
+def main():
+    print("---Bem vindo(a) a tela de cadastro de informações gerais---")
+    print("---Aqui você pode personalizar os seus gostos---")
+
+    #Chama a função que retorna user ou prof
+    #example.function()
+
+
+    #Estrutura de escolhas
+    if():
+        #Chama a função pegar_informacoes
+        informacoes_user.pegar_informacoes
+        pass
+    elif():
+        #Chama a função alterar_informacoes
+        informacoes_user.alterar_informacoes()
+        pass
+    else:
+        #Retorna o fim
+        print("-----")
+        return None
